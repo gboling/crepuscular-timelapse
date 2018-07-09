3c8b189602337ff5387e82f2eabdd572fe5cbf12
J. Grant Boling <gboling@gmail.com>
2018-07-08 21:09:42 -0700

    Fix if then logic to determine when to record

--- crepuscularTimelapse.py
+++ crepuscularTimelapse.py
@@ -23,13 +23,14 @@ rec_dusk = config["dusk"]
 rec_sunrise = config["sunrise"]
 rec_sunset = config["sunset"]
 rec_dict = {'Dawn':rec_dawn, 'Dusk':rec_dusk, 'Sunrise':rec_sunrise, 'Sunset':rec_sunset}
-print(rec_dict)
+# print(rec_dict)
 
 CAM_RESOLUTION = config["resolution"]
 camera = PiCamera()
 camera.resolution = (CAM_RESOLUTION)
 tl_interval = config["tl_interval"]
 rec_time = config["rec_time"]
+rolling = False
 
 print('Information for {0}/{1}'.format(city_name, city.region))
 print('Latitude: {0}\tLongitude{1}'.format(city.latitude, city.longitude))
@@ -43,6 +44,7 @@ def set_time():
     today = datetime.date.today()
     now = pytz.utc.localize(datetime.datetime.utcnow())
     sun = city.sun(date=today, local=False)
+    # rewrite the following to use the builtin sun_utc dict object
     dawn = sun['dawn']
     sunrise = sun['sunrise']
     noon = sun['noon']
@@ -60,20 +62,32 @@ def set_time():
     return sched_dict, now, today
 
 def tl_capture():
-    camera.capture_continuous('image{counter:04d}.jpg')
+    for filename in enumerate(
+            camera.capture_continuous('image{counter:04d}.jpg')):
+        (sched_dict, now, today) = set_time()
+        print('Image recorded to {0} at {1}[UTC]'.format(filename, now))
+        time.sleep(tl_interval)
+        roll = check_rolling()
+        if roll == False: break
     return
 
 def check_rolling():
     (sched_dict, now, today) = set_time()
-    rolling = False
-    if rec_dawn and now > sched_dict['dawn'] < sched_dict['rec_cutoff_dawn']:
+#    rolling = False
+    if rec_dawn and (sched_dict['dawn'] < now < sched_dict['rec_cutoff_dawn']):
         rolling = True
-    if rec_sunrise and now > sched_dict['sunrise'] < sched_dict['rec_cutoff_sunrise']:
+        print('Recording from {0} to {1}'.format(sched_dict['dawn'], sched_dict['rec_cutoff_dawn']))
+    elif rec_sunrise and (sched_dict['sunrise'] < now < sched_dict['rec_cutoff_sunrise']):
         rolling = True
-    if rec_dusk and now > sched_dict['dusk'] < sched_dict['rec_cutoff_dusk']:
+        print('Recording from {0} to {1}'.format(sched_dict['sunrise'], sched_dict['rec_cutoff_sunrise']))
+    elif rec_dusk and (sched_dict['dusk'] < now < sched_dict['rec_cutoff_dusk']):
         rolling = True
-    if rec_sunset and now > sched_dict['sunset'] > sched_dict['rec_cutoff_sunset']:
+        print('Recording from {0} to {1}'.format(sched_dict['dusk'], sched_dict['rec_cutoff_dusk']))
+    elif rec_sunset and (sched_dict['sunset'] < now < sched_dict['rec_cutoff_sunset']):
         rolling = True
+        print('Recording from {0} to {1}'.format(sched_dict['sunset'], sched_dict['rec_cutoff_sunset']))
+    else:
+        rolling = False
 
     return rolling
 
@@ -81,7 +95,7 @@ try:
     while True:
         rolling = check_rolling()
         if rolling: tl_capture()
-        time.sleep(tl_interval)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("Quit")
